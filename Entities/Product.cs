13134f39ed68eb1b6149f93c69d2cae850e555e4
RikiBenziman <rkb07841@GMAIL.com>
2023-11-01 16:30:35 +0200

    Start EF

---
+++ Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entities;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public int CategoryId { get; set; }
+
+    public string? ProductName { get; set; }
+
+    public double? ProductPrice { get; set; }
+
+    public string? ProductDescription { get; set; }
+
+    public string? ProductImage { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
--- Entities/User.cs
+++ Entities/User.cs
@@ -1,21 +1,17 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
 
-namespace Entities
+namespace Entities;
+
+public partial class User
 {
-    public class User
-    {
-        public int userId { get; set; }
-        public string UserName { get; set; }
-        [Required]
-        public string Password { get; set; }
-        [Required]
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public override string ToString()
-        {
-            return "{userName:" + UserName + "\nPassword:" + Password + "\nFirstName:" + FirstName + "\nLastName:" + LastName + "\n}";
-        }
+    public int UserId { get; set; }
 
+    public string? UserName { get; set; }
 
-    }
+    public string? Password { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
 }
